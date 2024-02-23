2cce9e31efb2fd0bf8734d930deda301838da8d9
atticusofsparta <atticusofsparta@protonmail.com>
2024-02-22 19:31:03 -0600

    fix(exports): update package exports to have index in src folder

--- examples/node/index.cjs
+++ examples/node/index.cjs
@@ -1,12 +1,12 @@
-const { ArIO } = require('../../lib/cjs/node');
+const { ArIO } = require('../../lib/cjs');
 
 (async () => {
   const arIO = new ArIO({});
-  const testnetClient = arIO.testnet;
-  const devnetClient = arIO.devnet;
+  const testnetContract = arIO.testnet;
+  const devnetContract = arIO.devnet;
 
-  const testnetGateways = await testnetClient.getGateways();
-  const devnetGateways = await devnetClient.getGateways();
+  const testnetGateways = await testnetContract.getGateways();
+  const devnetGateways = await devnetContract.getGateways();
 
   console.dir({ testnetGateways, devnetGateways }, { depth: 2 });
 })();
--- examples/node/index.mjs
+++ examples/node/index.mjs
@@ -1,12 +1,12 @@
-import { ArIO } from '../../lib/esm/node/index.js';
+import { ArIO } from '../../lib/esm/index.js';
 
 (async () => {
   const arIO = new ArIO({});
-  const testnetClient = arIO.testnet;
-  const devnetClient = arIO.devnet;
+  const testnetContract = arIO.testnet;
+  const devnetContract = arIO.devnet;
 
-  const testnetGateways = await testnetClient.getGateways();
-  const devnetGateways = await devnetClient.getGateways();
+  const testnetGateways = await testnetContract.getGateways();
+  const devnetGateways = await devnetContract.getGateways();
 
   console.dir({ testnetGateways, devnetGateways }, { depth: 2 });
 })();
