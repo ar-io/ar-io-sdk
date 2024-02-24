cc037ac5a3df2e76375679b8b14723c3b5d51fe2
atticusofsparta <atticusofsparta@protonmail.com>
2024-02-23 19:20:29 -0600

    fix(examples): update examples to use devnet

--- examples/node/index.cjs
+++ examples/node/index.cjs
@@ -1,11 +1,11 @@
-const { ArIO } = require('../../../lib/cjs/node/index.js');
+const { ArIO } = require('../../lib/cjs/node/index.js');
 
 (async () => {
   const arIO = new ArIO({});
   // testnet gateways
   const testnetGateways = await arIO.testnet.getGateways();
   // mainnet gateways
-  const devnetGateways = await arIO.mainnet.getGateways();
+  const devnetGateways = await arIO.devnet.getGateways();
 
   console.dir({ testnetGateways, devnetGateways }, { depth: 2 });
 })();
--- examples/node/index.mjs
+++ examples/node/index.mjs
@@ -5,7 +5,7 @@ import { ArIO } from '../../lib/esm/node/index.js';
   // testnet gateways
   const testnetGateways = await arIO.testnet.getGateways();
   // mainnet gateways
-  const devnetGateways = await arIO.mainnet.getGateways();
+  const devnetGateways = await arIO.devnet.getGateways();
 
   console.dir({ testnetGateways, devnetGateways }, { depth: 2 });
 })();
