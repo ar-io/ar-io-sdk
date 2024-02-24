c8b7998db824506c1e213557e859604d903aab9c
dtfiedler <dylan@ar.io>
2024-02-23 17:46:03 -0700

    fix(winston): move the winston polyfill - this will prevent any esm based web projects from getting polyfill issues

--- src/web/index.ts
+++ src/web/index.ts
@@ -14,9 +14,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-// polyfill for setImmediate
-import 'setimmediate';
-
 export * from '../types/index.js';
 export * from '../common/index.js';
 export * from '../constants.js';
