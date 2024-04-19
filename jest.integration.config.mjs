c1ad5ba7f019a6f772c063fc49713e2578805e52
dtfiedler <dylan@ar.io>
2024-04-19 07:01:00 -0600

    chore: revert jest config

--- jest.integration.config.mjs
+++ jest.integration.config.mjs
@@ -1,7 +1,7 @@
 export default {
   preset: 'ts-jest',
   // TODO: this should be using globalSetup once this is resolved: https://github.com/kulshekhar/ts-jest/issues/4127
-  globalSetup: './tests/integration/jest.setup.ts',
+  setupFiles: ['./tests/integration/jest.setup.ts'],
   globalTeardown: './tests/integration/jest.teardown.ts',
   clearMocks: true,
   moduleFileExtensions: ['ts', 'js', 'mjs'],
