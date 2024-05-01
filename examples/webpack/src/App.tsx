8d856fcd9dc345cff2a04f21a892a5090dd5f924
dtfiedler <dylan@ar.io>
2024-05-01 16:39:38 -0600

    chore(docks): offer a webpack and vite examples

---
+++ examples/webpack/src/App.tsx
@@ -0,0 +1,36 @@
+import { ANT } from '@ar.io/sdk';
+import { useEffect, useState } from 'react';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+import './App.css';
+
+const contractTxId = 'ilwT4ObFQ7cGPbW-8z-h7mvvWGt_yhWNlqxNjSUgiYY';
+const ant = ANT.init({
+  contractTxId,
+});
+function App() {
+  const [contract, setContract] = useState<string>('Loading...');
+
+  useEffect(() => {
+    ant
+      .getState()
+      .then((state) => {
+        setContract(`\`\`\`json\n${JSON.stringify(state, null, 2)}`);
+      })
+      .catch((error) => {
+        console.error(error);
+        setContract('Error loading contract state');
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Markdown className="markdown" remarkPlugins={[remarkGfm]}>
+        {contract}
+      </Markdown>
+    </div>
+  );
+}
+
+export default App;
