282090e0090387f0d757acfc79022c034fb59dee
dtfiedler <dylan@ar.io>
2024-05-02 09:20:08 -0600

    chore(docs): update vite example and remove alias in tsconfig
    The alias is nodoing what i thought it would.

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -1,27 +1,21 @@
+import { ANT, ANTState } from '@ar.io/sdk';
 import { useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { Contract, WarpFactory } from 'warp-contracts';
 
 import './App.css';
 
 const contractTxId = 'ilwT4ObFQ7cGPbW-8z-h7mvvWGt_yhWNlqxNjSUgiYY';
-const warp = WarpFactory.forMainnet();
-const antContract = warp.contract(contractTxId);
+const antContract = ANT.init({ contractTxId });
 
 function App() {
   const [contract, setContract] = useState<string>('Loading...');
 
   useEffect(() => {
     antContract
-      .syncState(`https://api.arns.app/v1/contract/${contractTxId}`, {
-        validity: true,
-      })
-      .then(async (syncContract: Contract) => {
-        const { cachedValue } = await syncContract.readState();
-        setContract(
-          `\`\`\`json\n${JSON.stringify(cachedValue.state, null, 2)}`,
-        );
+      .getState()
+      .then((state: ANTState) => {
+        setContract(`\`\`\`json\n${JSON.stringify(state, null, 2)}`);
       })
       .catch((error: unknown) => {
         console.error(error);
