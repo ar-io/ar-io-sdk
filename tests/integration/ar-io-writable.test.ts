45e3521f7701fff1be0870dcf2f8af65e68d1cf8
dtfiedler <dylan@ar.io>
2024-05-22 12:56:18 -0600

    chore(test): add better expectations on sdk tests

--- tests/integration/ar-io-writable.test.ts
+++ tests/integration/ar-io-writable.test.ts
@@ -4,18 +4,20 @@ import Transaction from 'arweave/node/lib/transaction.js';
 import { ArIO, ArIOWritable } from '../../src/common/ar-io.js';
 import { WarpContract } from '../../src/common/index.js';
 import { DefaultLogger } from '../../src/common/logger.js';
-import { ArIOState } from '../../src/contract-state.js';
-import { IOToken } from '../../src/token.js';
+import { ArIOState, ArNSLeaseData } from '../../src/contract-state.js';
+import { IOToken, mIOToken } from '../../src/token.js';
 import { arweave, gatewayAddress, localCacheUrl, warp } from '../constants.js';
 
 describe('ArIOWriteable', () => {
   let signer: ArweaveSigner;
+  let interactingAddress = '';
   let contractTxId: string;
   let arIO: ArIOWritable;
 
   beforeAll(() => {
     contractTxId = process.env.DEPLOYED_REGISTRY_CONTRACT_TX_ID!;
     signer = new ArweaveSigner(JSON.parse(process.env.PRIMARY_WALLET_JWK!));
+    interactingAddress = process.env.PRIMARY_WALLET_ADDRESS!;
     arIO = ArIO.init({
       signer,
       contract: new WarpContract<ArIOState>({
@@ -55,6 +57,12 @@ describe('ArIOWriteable', () => {
     expect(tx.id).toBeDefined();
     const verified = await arweave.transactions.verify(tx as Transaction);
     expect(verified).toBe(true);
+    const gateway = await arIO.getGateway({ address: gatewayAddress });
+    expect(gateway?.delegates[interactingAddress]).toEqual({
+      start: expect.any(Number),
+      delegatedStake: new IOToken(100).toMIO().valueOf(),
+      vaults: {},
+    });
   });
 
   it('should successfully decrease delegate stake', async () => {
@@ -65,9 +73,24 @@ describe('ArIOWriteable', () => {
     expect(tx.id).toBeDefined();
     const verified = await arweave.transactions.verify(tx as Transaction);
     expect(verified).toBe(true);
+    const gateway = await arIO.getGateway({ address: gatewayAddress });
+    expect(gateway?.delegates[interactingAddress]).toEqual({
+      delegatedStake: 0,
+      start: expect.any(Number),
+      vaults: {
+        [tx.id as string]: {
+          start: expect.any(Number),
+          end: expect.any(Number),
+          balance: new IOToken(100).toMIO().valueOf(),
+        },
+      },
+    });
   });
 
   it('should successfully transfer tokens to another address', async () => {
+    const balanceBefore = await arIO.getBalance({
+      address: interactingAddress,
+    });
     const target = ''.padEnd(43, 'f');
     const tx = await arIO.transfer({
       target,
@@ -76,11 +99,23 @@ describe('ArIOWriteable', () => {
     expect(tx.id).toBeDefined();
     const verified = await arweave.transactions.verify(tx as Transaction);
     expect(verified).toBe(true);
+    const targetBalance = await arIO.getBalance({ address: target });
+    expect(targetBalance).toEqual(new IOToken(10).toMIO().valueOf());
+    const balanceAfter = await arIO.getBalance({
+      address: interactingAddress,
+    });
+    const expectedAfter = new mIOToken(balanceBefore)
+      .minus(new IOToken(10).toMIO())
+      .valueOf();
+    expect(balanceAfter).toEqual(expectedAfter);
   });
 
   it('should successfully extend a domain', async () => {
     const domain = 'test-extend';
     const years = 2;
+    const recordBefore = (await arIO.getArNSRecord({
+      domain: 'test-extend',
+    })) as ArNSLeaseData;
     const tx = await arIO.extendLease({
       domain,
       years,
@@ -88,6 +123,10 @@ describe('ArIOWriteable', () => {
     expect(tx.id).toBeDefined();
     const verified = await arweave.transactions.verify(tx as Transaction);
     expect(verified).toBe(true);
+    const record = (await arIO.getArNSRecord({ domain })) as ArNSLeaseData;
+    expect(record?.endTimestamp).toBe(
+      recordBefore.endTimestamp + years * 31536000,
+    );
   });
 
   it('should successfully increase the undername support on a domain', async () => {
@@ -100,5 +139,7 @@ describe('ArIOWriteable', () => {
     expect(tx.id).toBeDefined();
     const verified = await arweave.transactions.verify(tx as Transaction);
     expect(verified).toBe(true);
+    const record = await arIO.getArNSRecord({ domain });
+    expect(record?.undernames).toBe(11);
   });
 });
