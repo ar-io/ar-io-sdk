5bb6845634acc9908afad8f0c3b8ca66cb6d74f4
dtfiedler <dylan@ar.io>
2024-05-24 15:48:50 -0600

    chore(test): revert url in test file

--- tests/constants.ts
+++ tests/constants.ts
@@ -3,7 +3,7 @@ import { LoggerFactory, WarpFactory } from 'warp-contracts';
 import { DeployPlugin } from 'warp-contracts-plugin-deploy';
 
 export const GATEWAY_PORT = process.env.GATEWAY_PORT ?? 1984;
-export const GATEWAY_HOST = process.env.GATEWAY_HOST ?? '127.0.0.1';
+export const GATEWAY_HOST = process.env.GATEWAY_HOST ?? 'localhost';
 export const GATEWAY_PROTOCOL = process.env.GATEWAY_PROTOCOL ?? 'http';
 
 // Arweave
@@ -22,4 +22,4 @@ export const evaluateToBlockHeight = 1;
 export const evaluateToSortKey =
   '000000000000,0000000000000,0000000000000000000000000000000000000000000000000000000000000000';
 export const localCacheUrl =
-  process.env.REMOTE_CACHE_URL || 'http://arns-service:3000';
+  process.env.REMOTE_CACHE_URL || 'http://localhost:3000';
