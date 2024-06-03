3cf0d6396bcf41720c17c5e8d6384e18cc9730af
dtfiedler <dylan@ar.io>
2024-06-03 09:26:40 -0600

    chore(example): update esm example to use IO

--- examples/esm/index.mjs
+++ examples/esm/index.mjs
@@ -1,43 +1,26 @@
-import { ArIO } from '@ar.io/sdk';
+import { IO, ioDevnetProcessId } from '@ar.io/sdk';
 
 (async () => {
-  // const arIO = ArIO.init({
-  //   contractTxId: ARNS_TESTNET_REGISTRY_TX,
-  // });
-  // // testnet gateways
-  // const testnetGateways = await arIO.getGateways();
-  // const protocolBalance = await arIO.getBalance({
-  //   address: ARNS_TESTNET_REGISTRY_TX,
-  // });
-  // const ardriveRecord = await arIO.getArNSRecord({ domain: 'ardrive' });
-  // const allRecords = await arIO.getArNSRecords();
-  // const oldEpoch = await arIO.getEpoch({
-  //   blockHeight: 1382230,
-  // });
-  // const epoch = await arIO.getCurrentEpoch();
-  // const observations = await arIO.getObservations();
-  // const observation = await arIO.getObservations({ epochStartHeight: 1350700 });
-  // const distributions = await arIO.getDistributions();
-  // console.dir(
-  //   {
-  //     testnetGateways,
-  //     ardriveRecord,
-  //     protocolBalance,
-  //     arnsStats: {
-  //       'registered domains': Object.keys(allRecords).length,
-  //       ardrive: allRecords.ardrive,
-  //     },
-  //     oldEpoch,
-  //     epoch,
-  //     observations,
-  //     observation,
-  //     distributions,
-  //   },
-  //   { depth: 2 },
-  // );
-  const process = ArIO.init({
+  const arIO = IO.init({
     processId: 'GaQrvEMKBpkjofgnBi_B3IgIDmY_XYelVLB6GcRGrHc',
   });
-  const records = await process.getArNSRecords();
-  console.log(records);
+  // // testnet gateways
+  const testnetGateways = await arIO.getGateways();
+  const protocolBalance = await arIO.getBalance({
+    address: ioDevnetProcessId,
+  });
+  const ardriveRecord = await arIO.getArNSRecord({ domain: 'ardrive' });
+  const allRecords = await arIO.getArNSRecords();
+  console.dir(
+    {
+      testnetGateways,
+      ardriveRecord,
+      protocolBalance,
+      arnsStats: {
+        'registered domains': Object.keys(allRecords).length,
+        ardrive: allRecords.ardrive,
+      },
+    },
+    { depth: 2 },
+  );
 })();
