5b87b9fc2c7ae71af6416d326b6a69c0b1eaad0f
dtfiedler <dylan@ar.io>
2024-06-06 17:37:14 -0600

    chore(examples): update esm examples

--- examples/esm/index.mjs
+++ examples/esm/index.mjs
@@ -5,32 +5,26 @@ import fs from 'fs';
   const wallet = JSON.parse(fs.readFileSync('./wallet.json', 'utf8'));
   const signer = new ArweaveSigner(wallet);
   const arIO = IO.init({
-    processId: 'GaQrvEMKBpkjofgnBi_B3IgIDmY_XYelVLB6GcRGrHc',
+    processId: ioDevnetProcessId,
     signer,
   });
-  // // testnet gateways
-  // const testnetGateways = await arIO.getGateways();
-  // const protocolBalance = await arIO.getBalance({
-  //   address: ioDevnetProcessId,
-  // });
-  // const ardriveRecord = await arIO.getArNSRecord({ domain: 'ardrive' });
-  // const allRecords = await arIO.getArNSRecords();
-  const transfer = await arIO.transfer({
-    target: 'ZjmB2vEUlHlJ7-rgJkYP09N5IzLPhJyStVrK5u9dDEo',
-    qty: 1000000000000,
+  // devnet gateways
+  const testnetGateways = await arIO.getGateways();
+  const protocolBalance = await arIO.getBalance({
+    address: ioDevnetProcessId,
   });
-
-  console.log(transfer);
-  // console.dir(
-  //   {
-  //     testnetGateways,
-  //     ardriveRecord,
-  //     protocolBalance,
-  //     arnsStats: {
-  //       'registered domains': Object.keys(allRecords).length,
-  //       ardrive: allRecords.ardrive,
-  //     },
-  //   },
-  //   { depth: 2 },
-  // );
+  const ardriveRecord = await arIO.getArNSRecord({ domain: 'ardrive' });
+  const allRecords = await arIO.getArNSRecords();
+  console.dir(
+    {
+      testnetGateways,
+      ardriveRecord,
+      protocolBalance,
+      arnsStats: {
+        'registered domains': Object.keys(allRecords).length,
+        ardrive: allRecords.ardrive,
+      },
+    },
+    { depth: 2 },
+  );
 })();
