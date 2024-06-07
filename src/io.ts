7e49131506dec2131ad2d3600706bf5a501ea77c
dtfiedler <dylan@ar.io>
2024-06-07 07:19:11 -0600

    chore: cleanup

--- src/io.ts
+++ src/io.ts
@@ -14,8 +14,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import Ar from 'arweave/node/ar.js';
-
 import { AOProcess } from './common/index.js';
 import {
   ArNSNameData,
