05e0673873490180e71974b90448561cacd21662
dtfiedler <dylan@ar.io>
2024-06-19 13:48:07 -0700

    fix(deps): use p-limit-lit to avoid jest issues

--- src/utils/graphql/processes.ts
+++ src/utils/graphql/processes.ts
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import pLimit from 'p-limit';
+import { pLimit } from 'plimit-lit';
 
 import { ANT } from '../../common/ant.js';
 import { IO } from '../../common/io.js';
