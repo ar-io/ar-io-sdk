1d50cd12a3ffd37bbf23a368c590b74aaa040b93
dtfiedler <dylan@ar.io>
2024-07-02 13:34:35 -0600

    fix(deps): add eventemitter3 dep

--- src/utils/graphql/processes.ts
+++ src/utils/graphql/processes.ts
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import EventEmitter from 'eventemitter3';
+import { EventEmitter } from 'eventemitter3';
 import { pLimit } from 'plimit-lit';
 
 import { ANT } from '../../common/ant.js';
