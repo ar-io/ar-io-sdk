38ca913fc9f935663b750e4f68e97879d3b70e5f
atticusofsparta <atticusofsparta@protonmail.com>
2024-08-06 08:56:46 -0600

    fix(tests): update web test to use ANT registry in app

--- tests/e2e/web/src/App.tsx
+++ tests/e2e/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { IO } from '@ar.io/sdk/web';
+import { ANTRegistry, IO } from '@ar.io/sdk/web';
 import { useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
@@ -6,24 +6,38 @@ import remarkGfm from 'remark-gfm';
 import './App.css';
 
 const io = IO.init();
+const antRegistry = ANTRegistry.init();
 function App() {
   const [contract, setContract] = useState<string>('Loading...');
+  const [ants, setAnts] = useState<string>('Loading...');
   const [success, setSuccess] = useState<boolean>(false);
   const [loaded, setLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     io.getInfo()
-      .then((state) => {
+      .then((state: any) => {
         setContract(`\`\`\`json\n${JSON.stringify(state, null, 2)}`);
         setSuccess(true);
       })
-      .catch((error) => {
+      .catch((error: any) => {
         console.error(error);
         setSuccess(false);
         setContract('Error loading contract state');
       })
       .finally(() => {
         setLoaded(true);
+      });
+    antRegistry
+      .accessControlList({
+        address: '7waR8v4STuwPnTck1zFVkQqJh5K9q9Zik4Y5-5dV7nk',
+      })
+      .then((affiliatedAnts: { Owned: string[]; Controlled: string[] }) => {
+        setAnts(`\`\`\`json\n${JSON.stringify(affiliatedAnts, null, 2)}`);
+      })
+      .catch((error: any) => {
+        console.error(error);
+        setSuccess(false);
+        setAnts('Error loading affiliated ants');
       });
   }, []);
 
@@ -32,6 +46,9 @@ function App() {
       {loaded && <div data-testid="load-info-result">{`${success}`}</div>}
       <Markdown className="markdown" remarkPlugins={[remarkGfm]}>
         {contract}
+      </Markdown>
+      <Markdown className="markdown" remarkPlugins={[remarkGfm]}>
+        {ants}
       </Markdown>
     </div>
   );
