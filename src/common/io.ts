a2976282247403b533ac129eaf76bd35c7af56cc
dtfiedler <dylan@ar.io>
2024-11-25 12:49:00 -0600

    fix(io): updated types and fixed apis for primary name requests
    These have been refined on the contract

--- src/common/io.ts
+++ src/common/io.ts
@@ -640,21 +640,14 @@ export class IOReadable implements AoIORead {
     });
   }
 
-  async getPrimaryNameRequest(
-    params:
-      | {
-          initiator: WalletAddress;
-        }
-      | {
-          name: string;
-        },
-  ): Promise<AoPrimaryNameRequest> {
+  async getPrimaryNameRequest(params: {
+    initiator: WalletAddress;
+  }): Promise<AoPrimaryNameRequest> {
     const allTags = [
       { name: 'Action', value: 'Primary-Name-Request' },
-      { name: 'Name', value: (params as { name: string }).name },
       {
         name: 'Initiator',
-        value: (params as { initiator: WalletAddress }).initiator,
+        value: params.initiator,
       },
     ];
 
