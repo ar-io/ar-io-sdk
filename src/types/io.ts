b6d574878de3016f72b6c5baa16b577c5d60a61e
dtfiedler <dylan@ar.io>
2025-01-03 16:45:13 -0600

    chore(docs): add deprecated to type

--- src/types/io.ts
+++ src/types/io.ts
@@ -628,7 +628,7 @@ export interface AoARIOWrite extends AoARIORead {
     options?: WriteOptions,
   ): Promise<AoMessageResult>;
   // END OF GATEWAY SPECIFIC INTERACTIONS
-  // @deprecated - use `buyArNSName` instead
+  /* @deprecated - use `buyArNSName` instead */
   buyRecord(
     params: AoBuyRecordParams,
     options?: WriteOptions,
@@ -637,7 +637,7 @@ export interface AoARIOWrite extends AoARIORead {
     params: AoArNSPurchaseParams,
     options?: WriteOptions,
   ): Promise<AoMessageResult>;
-  // @deprecated - use `upgradeArNSName` instead
+  /* @deprecated - use `upgradeArNSName` instead */
   upgradeRecord(
     params: AoArNSPurchaseParams,
     options?: WriteOptions,
