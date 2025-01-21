ec0e6768f4b329335d19f9bac7276d6538f5f358
Derek Sonnenberg <derek@ardrive.io>
2025-01-21 12:28:41 -0600

    test(errors): add unit coverage for ao error outputs PE-7417

--- src/common/contracts/ao-process.ts
+++ src/common/contracts/ao-process.ts
@@ -17,6 +17,7 @@ import { connect } from '@permaweb/aoconnect';
 
 import { AOContract, AoClient, AoSigner } from '../../types/index.js';
 import { getRandomText } from '../../utils/base64.js';
+import { errorMessageFromOutput } from '../../utils/index.js';
 import { safeDecode } from '../../utils/json.js';
 import { version } from '../../version.js';
 import { WriteInteractionError } from '../error.js';
@@ -236,41 +237,4 @@ export class AOProcess implements AOContract {
     }
     throw lastError;
   }
-}
-function errorMessageFromOutput(output: {
-  Error?: string;
-  Messages?: { Tags?: { name: string; value: string }[] }[];
-}): string | undefined {
-  const errorData = output.Error;
-
-  // Attempt to extract error details from Messages.Tags if Error is undefined
-  const error =
-    errorData ??
-    output.Messages?.[0]?.Tags?.find((tag) => tag.name === 'Error')?.value;
-
-  if (error !== undefined) {
-    // Consolidated regex to match and extract line number and AO error message or Error Tags
-    const match = error.match(/\[string "aos"]:(\d+):\s*(.+)/);
-    if (match) {
-      const [, lineNumber, errorMessage] = match;
-      const cleanError = removeUnicodeFromError(errorMessage);
-      return `${cleanError.trim()} (line ${lineNumber.trim()})`.trim();
-    }
-  }
-
-  return undefined;
-}
-
-function removeUnicodeFromError(error: string): string {
-  //The regular expression /[\u001b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g is designed to match ANSI escape codes used for terminal formatting. These are sequences that begin with \u001b (ESC character) and are often followed by [ and control codes.
-  const ESC = String.fromCharCode(27); // Represents '\u001b' or '\x1b'
-  return error
-    .replace(
-      new RegExp(
-        `${ESC}[\\[\\]()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]`,
-        'g',
-      ),
-      '',
-    )
-    .trim();
 }
