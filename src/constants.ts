8d442e0c6fe102a8890b4b38d4b37c434004842d
atticusofsparta <atticusofsparta@protonmail.com>
2025-01-24 12:02:18 -0600

    fix(ants): module bump _wSmbjfSlX3dZNcqE8JqKmj-DKum9uQ_jB08LwOKCyw

--- src/constants.ts
+++ src/constants.ts
@@ -39,7 +39,7 @@ export const ARIO_TESTNET_PROCESS_ID =
 
 export const ANT_REGISTRY_ID = 'i_le_yKKPVstLTDSmkHRqf-wYphMnwB9OhleiTgMkWc';
 export const MARIO_PER_ARIO = 1_000_000;
-export const AOS_MODULE_ID = 'BRyNPIJWZaQ4IudfNnsfvMrZBO2YDPjgKqg_wCYT2U8';
+export const AOS_MODULE_ID = '_wSmbjfSlX3dZNcqE8JqKmj-DKum9uQ_jB08LwOKCyw';
 export const ANT_LUA_ID = 'k9tQkbnFYZOGp6ist1yFuaqk_wOkzM5KUSNDtWzCLtg';
 
 export const AO_AUTHORITY = 'fcoN_xJeisVsPXA-trzVAuIiqO3ydLQxM-L4XbrQKzY';
