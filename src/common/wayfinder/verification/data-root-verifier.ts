4150e9e511c22576dc84b870057b067c2f9cd88b
dtfiedler <dylan@ar.io>
2025-05-02 16:38:32 -0500

    chore(wayfinder): remove unused data root function

--- src/common/wayfinder/verification/data-root-verifier.ts
+++ src/common/wayfinder/verification/data-root-verifier.ts
@@ -13,13 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {
-  Chunk,
-  MAX_CHUNK_SIZE,
-  MIN_CHUNK_SIZE,
-  buildLayers,
-  generateLeaves,
-} from 'arweave/node/lib/merkle.js';
 import { Readable } from 'node:stream';
 
 import { DataRootProvider, DataVerifier } from '../../../types/wayfinder.js';
@@ -28,43 +21,6 @@ import {
   hashReadableStreamToB64Url,
   hashReadableToB64Url,
 } from '../../../utils/hash.js';
-
-export async function convertBufferToDataRoot({
-  data,
-}: {
-  data: Buffer;
-}): Promise<string> {
-  const chunks: Chunk[] = [];
-  let cursor = 0;
-  let offset = 0;
-
-  while (offset < data.byteLength) {
-    let chunkSize = Math.min(MAX_CHUNK_SIZE, data.byteLength - offset);
-
-    const remainder = data.byteLength - offset - chunkSize;
-    if (remainder > 0 && remainder < MIN_CHUNK_SIZE) {
-      chunkSize = Math.ceil((data.byteLength - offset) / 2);
-    }
-
-    // subarray does not exist on web Buffer type
-    const slice = data.slice(offset, offset + chunkSize);
-    const hash = await crypto.subtle.digest('SHA-256', slice);
-    const hashArray = new Uint8Array(hash);
-
-    chunks.push({
-      dataHash: hashArray,
-      minByteRange: cursor,
-      maxByteRange: cursor + chunkSize,
-    });
-
-    cursor += chunkSize;
-    offset += chunkSize;
-  }
-  const leaves = await generateLeaves(chunks);
-  const result = await buildLayers(leaves);
-
-  return Buffer.from(result.id).toString('base64url');
-}
 
 export class DataRootVerifier implements DataVerifier {
   private readonly trustedDataRootProvider: DataRootProvider;
