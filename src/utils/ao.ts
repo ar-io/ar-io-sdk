a06544798f562356422ee13577abbb2661e4b616
dtfiedler <dylan@ar.io>
2025-07-14 09:30:26 -0500

    chore(test): add update test for error mesasge format parsing

--- src/utils/ao.ts
+++ src/utils/ao.ts
@@ -358,9 +358,11 @@ export function errorMessageFromOutput(output: {
 
   if (error !== undefined) {
     // Regex to match AO error messages like: [string ".src.main"]:5111: Primary name data not found
-    const match = error.match(/\[string "(?:\\.|[^"])*"\]:(\d+):\s*(.+)/);
+    // or [string "aos"]:128: some error
+    const match = error.match(/\[string "(.+)"\]:(\d+):\s*(.*)/);
     if (match) {
-      const [, lineNumber, errorMessage] = match;
+      // The first group is the src file, the second is the line number, and the third is the error message
+      const [, , lineNumber, errorMessage] = match;
       const cleanError = removeUnicodeFromError(errorMessage);
       return `${cleanError.trim()} (line ${lineNumber.trim()})`.trim();
     }
