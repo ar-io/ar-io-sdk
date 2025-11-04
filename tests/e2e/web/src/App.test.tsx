bd7c0ea65b6687ab9d87c02af8b0e2a6c46c9b6e
dtfiedler <dfieds913@gmail.com>
2025-11-04 15:05:45 -0600

    chore(test): remove test containers

---
+++ tests/e2e/web/src/App.test.tsx
@@ -0,0 +1,42 @@
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import { act } from 'react';
+import { describe, expect, it } from 'vitest';
+
+import App from './App';
+
+describe('ESM browser validation', () => {
+  it('should load the app and SDK', async () => {
+    await act(async () => render(<App />));
+
+    await waitFor(
+      () => {
+        screen.getByTestId('load-info-result');
+      },
+      {
+        interval: 5_000,
+        timeout: 120_000,
+      },
+    );
+
+    const result = screen.getByTestId('load-info-result');
+    expect(result).toHaveTextContent('true');
+  });
+
+  it('should retrieve ids from registry', async () => {
+    await act(async () => render(<App />));
+
+    await waitFor(
+      () => {
+        screen.getByTestId('load-registry-result');
+      },
+      {
+        interval: 5_000,
+        timeout: 120_000,
+      },
+    );
+
+    const result = screen.getByTestId('load-registry-result');
+    expect(result).toHaveTextContent('true');
+  });
+});
