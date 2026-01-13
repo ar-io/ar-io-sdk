0945b6e6cfc1ca0d1b5ceceb8ee9dcb0cb24a299
dtfiedler <dfieds913@gmail.com>
2026-01-13 15:00:33 -0600

    chore(git): use cu.ardrive.io for web tests

--- tests/e2e/web/src/App.tsx
+++ tests/e2e/web/src/App.tsx
@@ -25,8 +25,8 @@ const _defaultArIO = ARIO.init();
 // validating that the ANT registry works in web context
 const aoClient = connect({
   MODE: 'legacy',
-  // CU_URL: 'https://cu.ardrive.io', // TODO: use localhost when testing locally
-  CU_URL: 'http://localhost:6363',
+  CU_URL: 'https://cu.ardrive.io', // TODO: use localhost when testing locally
+  // CU_URL: 'http://localhost:6363',
 });
 const antRegistry = ANTRegistry.init({
   process: new AOProcess({
